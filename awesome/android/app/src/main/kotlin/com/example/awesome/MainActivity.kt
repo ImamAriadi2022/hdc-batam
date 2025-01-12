f5794586080b92cece1890e4972b88dfeff669f2
Imam ariadi <gamerapin139@gmail.com>
2025-01-12 16:44:57 +0700

    bersih2

--- awesome/android/app/src/main/kotlin/com/example/awesome/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.awesome
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- hdcfix/android/app/src/main/kotlin/com/example/hdcfix/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.hdcfix
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
